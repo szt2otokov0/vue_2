779fee6966eda8cfbed3121de1dd4642d1308adb
Ótott-Kovács Tamás <88544068+szt2otokov0@users.noreply.github.com>
2023-11-13 14:52:40 +0100

    added item deletion

--- form/src/components/skillset.vue
+++ form/src/components/skillset.vue
@@ -1,22 +1,25 @@
 <script setup>
 import { ref } from 'vue';
-const skills = ref([]);
+const skills = ref(new Set());
 const newSkill = ref(null);
 function addSkill(){
-    skills.value.push(newSkill.value)
+    skills.value.add(newSkill.value)
 }
-function clearSkills(){
-    skills.value = [];
+function deleteSkill(skill){
+    skills.value.delete(skill);
 }
 </script>
 
 <template>
     <h1>Képességek:</h1>
     <input type="text" v-model="newSkill">
-    <button @click="addSkill">Hozzáadás</button>
-    <button @click="clearSkills">Kiürítés</button>
+    <button @click="addSkill">Hozzáadás</button><br>
     <div class="card">
-        <h3>Képessgeim:</h3>
-        <div v-for="skill in skills">{{skill}}</div>
+        <h3>Képességeim:</h3>
+        <div v-for="skill in skills">
+            {{skill}}
+            <button @click="deleteSkill(skill)">Törlés</button>
+        </div>
+        
     </div>
 </template>
